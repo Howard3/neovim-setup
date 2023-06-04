f28c4609203aaa2ad3e662fd0cc32c4eb14e2a1e
Howard D. Lince III <git@h.lince.rocks>
2023-06-03 22:41:39 -0300

    copilot updates

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,19 @@
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.g.copilot_filetypes = {
+    ["*"] = false,
+    ["go"] = true,
+    ["javascript"] = true,
+    ["javascriptreact"] = true,
+    ["json"] = true,
+    ["lua"] = true,
+    ["python"] = true,
+    ["rust"] = true,
+    ["typescript"] = true,
+    ["typescriptreact"] = true,
+    ["vue"] = true,
+    ["yaml"] = true,
+    ["gohtml"] = true,
+    ["html"] = true,
+    ["gitcommit"] = true,
+}
