8fa994a7744728a38b6c0165df2e8fd0ac74f11c
Howard Lince III <howard.lince@infinit-o.com>
2023-06-06 01:05:55 -0300

    adding inline lsp errors

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -48,5 +48,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+	vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
 
 lsp.setup()
